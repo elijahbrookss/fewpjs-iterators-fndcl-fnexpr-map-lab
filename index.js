842778be850b6f16a75b051bcf0f5b5f2e68fb02
elijahbrookss <elijahcbrooks@gmail.com>
2020-11-18 16:15:34 -0600

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,29 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+const keyWords = {
+  oo: "OO",
+  api: "API",
+  "jsonp?": "JSONP?",
+  nan: "NaN",
+  stoppropagation: "StopPropagation",
+  "preventdefault?": "PreventDefault?"
+}
+
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    if (!keyWords[str[i]]){
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }else {
+      str[i] = keyWords[str[i]];
+    }
+  }
+  return str.join(' ');
+}
 
 const titleCased = () => {
-  return tutorials
+  let newTutorials = tutorials.map(tutorial => titleCase(tutorial))
+
+  return newTutorials;
 }
